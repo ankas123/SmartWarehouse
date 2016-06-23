d0a3b31f9d1960026fc08147377ee6c9d22d6742
ankas123 <anant2.1317@gmail.com>
2016-06-23 17:04:21 +0530

    Products

--- app/src/main/java/com/gaia/app/smartwarehouse/Additem.java
+++ app/src/main/java/com/gaia/app/smartwarehouse/Additem.java
@@ -1,21 +1,24 @@
 package com.gaia.app.smartwarehouse;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.gaia.app.smartwarehouse.classes.Userdata;
+import com.gaia.app.smartwarehouse.service.AddItemTask;
 
 /**
  * Created by anant on 23/06/16.
  */
 
 public class Additem extends AppCompatActivity {
-    private EditText name;
-    private EditText unit;
 
-
+    String email;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +32,31 @@ public class Additem extends AppCompatActivity {
         Intent intent=getIntent();
         Bundle bundle=intent.getBundleExtra("cname");
         String cname =(String) bundle.get("cname");
+        EditText editTexttname = (EditText) findViewById(R.id.additem_edittext);
+        EditText editTextunit = (EditText) findViewById(R.id.additem_edittext1);
+        String name =  editTexttname.getText().toString().trim();
+        String unit = editTextunit.getText().toString().trim();
+        Userdata details =new Userdata(this);
+        Cursor cursor=details.getdata();
 
+        if(cursor.moveToFirst()) {
+            do {
+                email = cursor.getString(0);
+
+            } while (cursor.moveToNext());
+        }
+
+        new AddItemTask(new AddItemTask.Afteradd() {
+            @Override
+            public void getMessage(String s) {
+                switch (s){
+                    case "100":
+                        break;
+                    default:
+                        Toast.makeText(Additem.this,"Error in sending",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).execute(email,name,unit,cname);
 
     }
 }
--- app/src/main/java/com/gaia/app/smartwarehouse/service/AddItemTask.java
+++ app/src/main/java/com/gaia/app/smartwarehouse/service/AddItemTask.java
@@ -2,6 +2,9 @@ package com.gaia.app.smartwarehouse.service;
 
 import android.os.AsyncTask;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -24,18 +27,23 @@ import static com.gaia.app.smartwarehouse.service.CommonUtilities.ADDITEM_URL;
 
 public class AddItemTask extends AsyncTask<String,Void,String> {
 
-    private String name,unit,cat,JSON_STRING;
-
+    public AddItemTask(Afteradd work) {
+        super();
+        this.work=work;
+    }
 
     public interface Afteradd{
-
+        void getMessage(String s);
     }
+
+    private Afteradd work = null;
 
     @Override
     protected String doInBackground(String... strings) {
-        name = strings[0];
-        unit = strings[1];
-        cat = strings[2];
+        String email = strings[0];
+        String name = strings[1];
+        String unit = strings[2];
+        String cat = strings[3];
 
         try {
 
@@ -48,6 +56,7 @@ public class AddItemTask extends AsyncTask<String,Void,String> {
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
 
             String data = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8")
+                    +URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8")
                     + URLEncoder.encode("unit", "UTF-8") + "=" + URLEncoder.encode(unit, "UTF-8")
                     + URLEncoder.encode("cat", "UTF-8") + "=" + URLEncoder.encode(cat, "UTF-8") ;
             bufferedWriter.write(data);
@@ -58,6 +67,7 @@ public class AddItemTask extends AsyncTask<String,Void,String> {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "ISO-8859-1"));
             StringBuilder stringBuilder = new StringBuilder();
 
+            String JSON_STRING;
             while ((JSON_STRING = bufferedReader.readLine()) != null) {
                 stringBuilder.append(JSON_STRING + "\n");
             }
@@ -84,5 +94,14 @@ public class AddItemTask extends AsyncTask<String,Void,String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+        try {
+            JSONObject object = new JSONObject(s);
+            String TAG_RESULT = "message";
+            String message = object.getString(TAG_RESULT);
+            work.getMessage(message);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
