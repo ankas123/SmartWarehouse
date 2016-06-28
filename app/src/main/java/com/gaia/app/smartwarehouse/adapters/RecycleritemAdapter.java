543ed45956bdf6e52141314687fc41263efad093
ankas123 <anant2.1317@gmail.com>
2016-06-28 12:15:17 +0530

    Products

--- app/src/main/java/com/gaia/app/smartwarehouse/adapters/RecycleritemAdapter.java
+++ app/src/main/java/com/gaia/app/smartwarehouse/adapters/RecycleritemAdapter.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gaia.app.smartwarehouse.DetailActivity;
@@ -34,11 +36,12 @@ public class RecycleritemAdapter extends RecyclerView.Adapter<RecycleritemAdapte
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView textView;
         private CardView card;
+        private ImageView fill;
         public ViewHolder(View itemView) {
             super(itemView);
             card = (CardView) itemView.findViewById(R.id.cv2);
             textView= (TextView)itemView.findViewById(R.id.itemtext);
-
+            fill = (ImageView) itemView.findViewById(R.id.itcardrec);
             card.setOnClickListener(this);
 
         }
@@ -63,6 +66,17 @@ public class RecycleritemAdapter extends RecyclerView.Adapter<RecycleritemAdapte
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.textView.setText(dataarray.get(position).getIname().trim());
         Integer in =dataarray.size();
+        String weight = dataarray.get(position).getWeight();
+
+        if(weight.equals("null")) {
+            holder.fill.getLayoutParams().height = 0;
+
+        }
+        else{
+
+            Float value = new Float(weight);
+            holder.fill.getLayoutParams().height=getPercentHeight(value);
+        }
         Log.v("size",in.toString());
 
     }
@@ -72,6 +86,14 @@ public class RecycleritemAdapter extends RecyclerView.Adapter<RecycleritemAdapte
 
         return dataarray.size();
     }
+    int getPercentHeight(float weight){
 
+        int height;
+        height= (int) ((weight/120)*50);
+        int dimension = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height, context.getResources().getDisplayMetrics());
+        return dimension;
+
+
+    }
 
 }
